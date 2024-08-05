f5503658a62382ed766a54f413eeb6249736a0a0
ahn-sj <jaesa5221@gmail.com>
2024-08-05 23:59:43 +0900

    [#1] feat: 멀티 모듈 적용

---
+++ api/common/src/test/java/com/tally/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tally.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
