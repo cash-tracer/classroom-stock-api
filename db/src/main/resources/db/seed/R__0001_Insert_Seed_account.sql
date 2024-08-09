48818e7093d4ed5d0c5af6fd0cd3390605f14fec
ahn-sj <jaesa5221@gmail.com>
2024-08-10 00:21:11 +0900

    [#2] feat: postgres flyway dml 추가 (seed data)

---
+++ db/src/main/resources/db/seed/R__0001_Insert_Seed_account.sql
@@ -0,0 +1,11 @@
+INSERT INTO accounts (email, password, created_at, updated_at)
+VALUES ('user1@example.com', 'hashed_password1', '2024-08-10 10:00:00', '2024-08-10 10:00:00'),
+       ('user2@example.com', 'hashed_password2', '2024-08-10 10:05:00', '2024-08-10 10:05:00'),
+       ('user3@example.com', 'hashed_password3', '2024-08-10 10:10:00', '2024-08-10 10:10:00'),
+       ('user4@example.com', 'hashed_password4', '2024-08-10 10:15:00', '2024-08-10 10:15:00'),
+       ('user5@example.com', 'hashed_password5', '2024-08-10 10:20:00', '2024-08-10 10:20:00'),
+       ('user6@example.com', 'hashed_password6', '2024-08-10 10:25:00', '2024-08-10 10:25:00'),
+       ('user7@example.com', 'hashed_password7', '2024-08-10 10:30:00', '2024-08-10 10:30:00'),
+       ('user8@example.com', 'hashed_password8', '2024-08-10 10:35:00', '2024-08-10 10:35:00'),
+       ('user9@example.com', 'hashed_password9', '2024-08-10 10:40:00', '2024-08-10 10:40:00'),
+       ('user10@example.com', 'hashed_password10', '2024-08-10 10:45:00', '2024-08-10 10:45:00');
